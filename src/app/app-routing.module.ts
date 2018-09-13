44e3c7cef8e9e3ec77cc7389741523f137308c26
crashe24 <crashe24@hotmail.com>
2018-09-13 15:08:14 -0500

    ingreso egreso

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { dashBoardRoutes } from './dashboard/dashboard.routes';
+
+const routes: Routes = [
+        {path: 'login', component: LoginComponent},
+        {path: 'register', component: RegisterComponent},
+        {
+            path: '', component: DashboardComponent,
+            children: dashBoardRoutes
+        },
+        {path: '**', redirectTo: ''}
+
+];
+
+
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot ( routes)
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+// sirve para importar en el app.module para decirle que ahora dispone de estas rutas
+export class AppRountigModule {
+
+}
